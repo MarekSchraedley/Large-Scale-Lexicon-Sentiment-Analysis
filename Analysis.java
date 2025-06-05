882bc57f61bd7e54ebfee1d0168fa939cdd5bb0e
MarekSchraedley <112725894+MarekSchraedley@users.noreply.github.com>
2025-06-04 20:40:39 -0500

    Update Analysis.java

--- Analysis.java
+++ Analysis.java
@@ -10,16 +10,6 @@ import edu.stanford.nlp.pipeline.*;
 import java.util.Properties;
 
 public class Analysis {
-    public static class ArticleItem {
-        private String myCountry;
-        private String myContents;
-        public ArticleItem(String country, String contents) {
-            myCountry = country;
-            myContents = contents;
-        }
-        public String getMyCountry() {return  myCountry;}
-        public String getMyContents() {return  myContents;}
-    }
     public static class word {
         private String myPos;
         private Double myPosSentiment;
@@ -35,27 +25,6 @@ public class Analysis {
     }
     public static void main(String[] args) {
         try {
-            var file = new Scanner(new File("Langdat/output.txt"));
-            String tempCountry = file.nextLine().substring(1);
-            String tempContents = "";
-            ArrayList<ArticleItem> list = new ArrayList<>();
-            while (file.hasNext()) {
-                String line = file.nextLine();
-                if (line != null && line.length() > 0) {
-                    if (line.substring(0, 1).equals("#")) {
-                        tempCountry = line.substring(1);
-                        list.add(new ArticleItem(tempCountry, tempContents.toLowerCase()));
-                        tempContents = "";
-                    } else {
-                        tempContents += line;
-                    }
-                }
-            }
-            file.close();
-
-             String[] ukraineRef = {"ukraine", "zelensky", "ukrainian"};
-             String[] russiaRef = {"russia", "putin"};
-
             var WordNetScanner = new Scanner(new File("Langdat/SentiWordNet_3.0.0.txt"));
             Map<String, word> WordMap = new HashMap<>();
             while (WordNetScanner.hasNext()) {
@@ -69,17 +38,14 @@ public class Analysis {
                     i++;
                 }
             }
-            System.out.println(WordMap.get("sparkle#1").getMyPosSentiment());
             var CommonScentences = new Scanner(new File("Langdat/yelp_labelled.txt"));
             double successes = 0.0;
             int total = 0;
             String[] negation = {"no", "not", "neither", "never", "none", "nothing", "nor", "nobody", "doesnt", "havent", "nowhere", "wasnt", "dont", "wont", "cant", "never", "arent", "isnt", "werent", "couldnt", "mustnt", "shouldnt", "wouldnt", "didnt", "hasnt", "havent", "hadnt", "lack", "without", "hardly", "barely", "scarcely", "fail"};
-
             while (CommonScentences.hasNext()) {
                 String[] line = CommonScentences.nextLine().split("\\s+");
 
                 for (int i = 0; i < line.length-1; i++) {
-                    //System.out.print(line[i]);
                     line[i] = line[i].toLowerCase().replaceAll("\\p{Punct}", "");
                 }
                 int score = Integer.parseInt(line[line.length-1]);
@@ -87,18 +53,14 @@ public class Analysis {
                 double average = 0.0;
                 double value = 0.0;
                 boolean negated = false;
+                /*  PRINT LINES AND SCORES
                 for (int i = 0; i < line.length-1; i++) {
-                    System.out.print(line[i]);
+                    System.out.print(line[i].substring(1));
                     if (WordMap.get(line[i] + "#1") != null) {
                         System.out.print(WordMap.get(line[i] + "#1").getMyPosSentiment() + "|" + WordMap.get(line[i] + "#1").getMyNegSentiment());
                     }
                 }
-
-                /*for (int i = 0; i < line.length-1; i++) {
-                    if (WordMap.get(line[i] + "#1") != null) {
-                        average += (WordMap.get(line[i] + "#1").getMyPosSentiment() - WordMap.get(line[i] + "#1").getMyNegSentiment());
-                    }
-                }*/
+                */
                 for (int i = 0; i < line.length-1; i++) {
                     value = 0.0;
                     negated = false;
@@ -124,16 +86,14 @@ public class Analysis {
                 System.out.print(score + " " + average);
                 if ((int)(average+1) == score && average != 0.0) {
                     successes++;
-                    System.out.print(" success");
+                    //System.out.print(" success"); Print Successes
                 }
                 if (average != 0.0) {
                     total++;
                 }
-                System.out.println();
+                //System.out.println(); Spacing
             }
-            System.out.println(successes/total);
-            System.out.println(WordMap.get("awful#1").getMyPosSentiment());
-            System.out.println(WordMap.get("awful#1").getMyNegSentiment());
+            System.out.println(successes/total); // percent accuracy
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
